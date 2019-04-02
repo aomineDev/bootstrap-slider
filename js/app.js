811e99783fef4e4c9b8bbd30036dc313f5f647de
aomine <xomarxpl07@outlook.es>
2019-04-02 16:34:57 -0500

    v 1.3

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ $sliderItem = document.querySelectorAll('.slider-item'),
 $controller = document.querySelector('.controller');
 // Slider Vars
 var sliderItemCount = $sliderItem.length - 1,
-SliderNow = 0,
-SliderNP = 0,
-SliderIndex = 0,
+sliderNow = 0,
+sliderNP = 0,
+sliderIndex = 0,
 // Create Controller bar
 controllerIndex = 0,
 controllerBar,
@@ -31,28 +31,28 @@ for (let i of $controllerBar) {
 }
 
 function showNextSlider(){
-	if (!$sliderItem[SliderNow].classList.contains('slider-marker')) {
-		if (SliderNow == sliderItemCount) {
-			SliderNP = 0;
+	if (!$sliderItem[sliderNow].classList.contains('slider-marker')) {
+		if (sliderNow == sliderItemCount) {
+			sliderNP = 0;
 		}else {
-			SliderNP = SliderNow + 1;
+			sliderNP = sliderNow + 1;
 		}
-		$sliderItem[SliderNow].classList.add('slider-marker');
-		$sliderItem[SliderNP].classList.add('slider-item-next');
+		$sliderItem[sliderNow].classList.add('slider-marker');
+		$sliderItem[sliderNP].classList.add('slider-item-next');
 		setTimeout(()=>{
-			$sliderItem[SliderNow].classList.add('slider-item-left');
-			$sliderItem[SliderNP].classList.add('slider-item-left');
-			$controllerBar[SliderNow].classList.remove('controller-active');
-			$controllerBar[SliderNP].classList.add('controller-active');
+			$sliderItem[sliderNow].classList.add('slider-item-left');
+			$sliderItem[sliderNP].classList.add('slider-item-left');
+			$controllerBar[sliderNow].classList.remove('controller-active');
+			$controllerBar[sliderNP].classList.add('controller-active');
 		}, 100);
 		setTimeout(()=>{
-			$sliderItem[SliderNP].classList.add('slider-active');
-			$sliderItem[SliderNP].classList.remove('slider-item-next', 'slider-item-left');
-			$sliderItem[SliderNow].classList.remove('slider-active', 'slider-item-left', 'slider-marker');
-			if(SliderNow == sliderItemCount){
-				SliderNow = 0;
+			$sliderItem[sliderNP].classList.add('slider-active');
+			$sliderItem[sliderNP].classList.remove('slider-item-next', 'slider-item-left');
+			$sliderItem[sliderNow].classList.remove('slider-active', 'slider-item-left', 'slider-marker');
+			if(sliderNow == sliderItemCount){
+				sliderNow = 0;
 			}else {
-				SliderNow++;
+				sliderNow++;
 			}
 			clearInterval(sliderAutoPlay);
 			sliderAutoPlay = setInterval(showNextSlider, 5000);
@@ -61,28 +61,28 @@ function showNextSlider(){
 }
 
 function showPrevSlider(){
-	if (!$sliderItem[SliderNow].classList.contains('slider-marker')) {
-		if (SliderNow == 0) {
-			SliderNP = sliderItemCount;
+	if (!$sliderItem[sliderNow].classList.contains('slider-marker')) {
+		if (sliderNow == 0) {
+			sliderNP = sliderItemCount;
 		}else {
-			SliderNP = SliderNow - 1;
+			sliderNP = sliderNow - 1;
 		}
-		$sliderItem[SliderNow].classList.add('slider-marker');
-		$sliderItem[SliderNP].classList.add('slider-item-prev');
+		$sliderItem[sliderNow].classList.add('slider-marker');
+		$sliderItem[sliderNP].classList.add('slider-item-prev');
 		setTimeout(()=>{
-			$sliderItem[SliderNow].classList.add('slider-item-right');
-			$sliderItem[SliderNP].classList.add('slider-item-right');
-			$controllerBar[SliderNow].classList.remove('controller-active');
-			$controllerBar[SliderNP].classList.add('controller-active');
+			$sliderItem[sliderNow].classList.add('slider-item-right');
+			$sliderItem[sliderNP].classList.add('slider-item-right');
+			$controllerBar[sliderNow].classList.remove('controller-active');
+			$controllerBar[sliderNP].classList.add('controller-active');
 		}, 100);
 		setTimeout(()=>{
-			$sliderItem[SliderNP].classList.add('slider-active');
-			$sliderItem[SliderNP].classList.remove('slider-item-prev', 'slider-item-right');
-			$sliderItem[SliderNow].classList.remove('slider-active', 'slider-item-right', 'slider-marker');
-			if(SliderNow == 0){
-				SliderNow = sliderItemCount;
+			$sliderItem[sliderNP].classList.add('slider-active');
+			$sliderItem[sliderNP].classList.remove('slider-item-prev', 'slider-item-right');
+			$sliderItem[sliderNow].classList.remove('slider-active', 'slider-item-right', 'slider-marker');
+			if(sliderNow == 0){
+				sliderNow = sliderItemCount;
 			}else {
-				SliderNow--;
+				sliderNow--;
 			}
 			clearInterval(sliderAutoPlay);
 			sliderAutoPlay = setInterval(showNextSlider, 5000);
@@ -91,39 +91,39 @@ function showPrevSlider(){
 }
 
 function controllerSlider() {
-	if (!$sliderItem[SliderNow].classList.contains('slider-marker')) {
-		SliderIndex = parseInt(this.dataset.index);
-		if (SliderNow != SliderIndex && SliderNow  < SliderIndex) {
-			$sliderItem[SliderNow].classList.add('slider-marker');
-			$sliderItem[SliderIndex].classList.add('slider-item-next');
+	if (!$sliderItem[sliderNow].classList.contains('slider-marker')) {
+		sliderIndex = parseInt(this.dataset.index);
+		if (sliderNow != sliderIndex && sliderNow  < sliderIndex) {
+			$sliderItem[sliderNow].classList.add('slider-marker');
+			$sliderItem[sliderIndex].classList.add('slider-item-next');
 			setTimeout(()=>{
-				$sliderItem[SliderNow].classList.add('slider-item-left');
-				$sliderItem[SliderIndex].classList.add('slider-item-left');
-				$controllerBar[SliderNow].classList.remove('controller-active');
-				$controllerBar[SliderIndex].classList.add('controller-active');
+				$sliderItem[sliderNow].classList.add('slider-item-left');
+				$sliderItem[sliderIndex].classList.add('slider-item-left');
+				$controllerBar[sliderNow].classList.remove('controller-active');
+				$controllerBar[sliderIndex].classList.add('controller-active');
 			}, 100);
 			setTimeout(()=>{
-				$sliderItem[SliderIndex].classList.add('slider-active');
-				$sliderItem[SliderIndex].classList.remove('slider-item-next', 'slider-item-left');
-				$sliderItem[SliderNow].classList.remove('slider-active', 'slider-item-left', 'slider-marker');
-				SliderNow = SliderIndex;
+				$sliderItem[sliderIndex].classList.add('slider-active');
+				$sliderItem[sliderIndex].classList.remove('slider-item-next', 'slider-item-left');
+				$sliderItem[sliderNow].classList.remove('slider-active', 'slider-item-left', 'slider-marker');
+				sliderNow = sliderIndex;
 				clearInterval(sliderAutoPlay);
 				sliderAutoPlay = setInterval(showNextSlider, 5000);
 			}, 700);
-		}else if(SliderNow != SliderIndex && SliderNow  > SliderIndex) {
-			$sliderItem[SliderNow].classList.add('slider-marker');
-			$sliderItem[SliderIndex].classList.add('slider-item-prev');
+		}else if(sliderNow != sliderIndex && sliderNow  > sliderIndex) {
+			$sliderItem[sliderNow].classList.add('slider-marker');
+			$sliderItem[sliderIndex].classList.add('slider-item-prev');
 			setTimeout(()=>{
-				$sliderItem[SliderNow].classList.add('slider-item-right');
-				$sliderItem[SliderIndex].classList.add('slider-item-right');
-				$controllerBar[SliderNow].classList.remove('controller-active');
-				$controllerBar[SliderIndex].classList.add('controller-active');
+				$sliderItem[sliderNow].classList.add('slider-item-right');
+				$sliderItem[sliderIndex].classList.add('slider-item-right');
+				$controllerBar[sliderNow].classList.remove('controller-active');
+				$controllerBar[sliderIndex].classList.add('controller-active');
 			}, 100);
 			setTimeout(()=>{
-				$sliderItem[SliderIndex].classList.add('slider-active');
-				$sliderItem[SliderIndex].classList.remove('slider-item-prev', 'slider-item-right');
-				$sliderItem[SliderNow].classList.remove('slider-active', 'slider-item-right', 'slider-marker');
-				SliderNow = SliderIndex;
+				$sliderItem[sliderIndex].classList.add('slider-active');
+				$sliderItem[sliderIndex].classList.remove('slider-item-prev', 'slider-item-right');
+				$sliderItem[sliderNow].classList.remove('slider-active', 'slider-item-right', 'slider-marker');
+				sliderNow = sliderIndex;
 				clearInterval(sliderAutoPlay);
 				sliderAutoPlay = setInterval(showNextSlider, 5000);
 			}, 700);
